9b4aa74c634c713c87f44fcf27b3df5badf6282a
Billy Chan <bill0930@gmail.com>
2021-05-05 16:16:42 +0800

    Initial Commit

---
+++ LocationListing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationListing
+//
+//  Created by Billy Chan on 5/5/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
